d565662a3134ceb0c717785f19da200b27ac834f
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2022-11-04 14:40:13 +0700

    modified:   src/server.js 	modified:   src/server2.js

--- src/server.js
+++ src/server.js
@@ -11,34 +11,39 @@ const producerService = ProducerService
 async function runProcess (hostname, dataDb, dataApi) {
     const database = dataDb.filter(obj => obj.hostname === hostname)
     const api = dataApi.computers.filter(obj => obj.hostName === hostname)
-    const apiFilter = api.map((data) => {
-        return {
-            hostname: data.hostName,
-            agentstatus: data.computerStatus.agentStatus,
-            agentmessage: data.computerStatus.agentStatusMessages,
-            lastcommunication: data.lastAgentCommunication
+
+    if (api[0] !== undefined) {
+        const apiFilter = api.map((data) => {
+            return {
+                hostname: data.hostName,
+                agentstatus: data.computerStatus.agentStatus,
+                agentmessage: data.computerStatus.agentStatusMessages,
+                lastcommunication: data.lastAgentCommunication
+            }
+        })
+        const stringMessage = apiFilter[0].agentmessage.toString()
+
+        if (database[0].agentstatus !== apiFilter[0].agentstatus || database[0].agentmessage !== stringMessage) {
+            const statusupdate = new Date().toISOString()
+            const lastcommunication = new Date(apiFilter[0].lastcommunication).toLocaleString()
+
+            await databaseService.updateAgentStatusServer(apiFilter[0].agentstatus, stringMessage, apiFilter[0].hostname, statusupdate)
+
+            const message = {
+                hostname: database[0].hostname,
+                displayname: database[0].displayname,
+                agentstatus: apiFilter[0].agentstatus,
+                agentmessage: stringMessage,
+                statusupdate,
+                lastcommunication,
+                adminmail: database[0].email
+            }
+            console.log(lastcommunication)
+            await databaseService.insertLog(database[0].server_id, apiFilter[0].agentstatus, stringMessage, statusupdate)
+            await producerService.sendMessage('sendEmail:notif', JSON.stringify(message))
         }
-    })
-    const stringMessage = apiFilter[0].agentmessage.toString()
-
-    if (database[0].agentstatus !== apiFilter[0].agentstatus || database[0].agentmessage !== stringMessage) {
-        const statusupdate = new Date().toISOString()
-        const lastcommunication = new Date(apiFilter[0].lastcommunication).toLocaleString()
-
-        await databaseService.updateAgentStatusServer(apiFilter[0].agentstatus, stringMessage, apiFilter[0].hostname, statusupdate)
-
-        const message = {
-            hostname: database[0].hostname,
-            displayname: database[0].displayname,
-            agentstatus: apiFilter[0].agentstatus,
-            agentmessage: stringMessage,
-            statusupdate,
-            lastcommunication,
-            adminmail: database[0].email
-        }
-        console.log(lastcommunication)
-        await databaseService.insertLog(database[0].server_id, apiFilter[0].agentstatus, stringMessage, statusupdate)
-        await producerService.sendMessage('sendEmail:notif', JSON.stringify(message))
+    } else {
+        console.log(hostname + ' is not found')
     }
 }
 
